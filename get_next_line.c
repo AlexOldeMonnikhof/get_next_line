c9a6c634c2fa90aea54eaff4bf39ae26069570d5
Jakko023 <70701009+Jakko023@users.noreply.github.com>
2022-11-23 17:33:05 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:57:12 by aolde-mo          #+#    #+#             */
-/*   Updated: 2022/11/23 16:17:41 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:32:32 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,33 @@
 
 // char	*make_return_line()
 
-// char	*make_buff(char *str)
+char	*make_ret(char *buff, int fd)
+{
+	int	bytes_read;
 
+	bytes_read = 69;
+	while ()	
+}
 char	*get_next_line(int fd)
 {
 	static char	*line;
 	char		*ret;
 	char		*buff;
-	int			bytes;
 
 	if (read(fd, 0, 0) || BUFFER_SIZE <= 0)
 		return (NULL);
-	buff = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	buff = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!buff)
 		return (NULL);
-	// buff = (fd)
-	// line = make_line
+	ret = make_buff(buff, fd)
+	// line = make_line();
 	printf("%zd", read(fd, buff, BUFFER_SIZE));
 	return (line);
 }
 
 
-int main(void)
-{
-	int fd = open("test.c", O_RDONLY);
-	get_next_line(fd);
-}
+// int main(void)
+// {
+// 	int fd = open("test.c", O_RDONLY);
+// 	get_next_line(fd);
+// }
--- test.c
+++ test.c
@@ -6,21 +6,20 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 14:36:48 by aolde-mo          #+#    #+#             */
-/*   Updated: 2022/11/23 14:47:56 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:32:35 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 int main(void)
 {
 	int len = 5;
 	char *test;
-
-	test = ft_calloc(len + 1, sizeof(char));
-	if (!test)
-		printf("faal\n");
-	else
-		printf("nice\n");
+	int fd = open("text.txt", O_RDONLY);
+	test = calloc(len + 1, sizeof(char));
+	read(fd, test, 100);
+	printf("%s", test);
 }
