8857e0140b0a96dad218ea0623c295162e18e0e0
Jakko023 <70701009+Jakko023@users.noreply.github.com>
2023-01-08 15:45:50 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:57:12 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/01/04 19:31:06 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2023/01/08 15:45:20 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,32 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-static char	*make_output_line(char *return_line)
+char	*make_output_line(char *raw_line)
 {
 	size_t	i;
-	size_t	length_new_line;
 	char	*new_line;
 
 	i = 0;
-	while (return_line[i] != '\n' && return_line[i])
-		i++;
-	if (return_line[i] == '\0')
-	{
-		// printf("hoi\t");
+	if (!raw_line[0])
 		return (NULL);
-	}
-	length_new_line = i + 1;
-	new_line = (char *)malloc((length_new_line + 1) * sizeof(char));
+	while (raw_line[i] != '\n' && raw_line[i])
+		i++;
+	if (raw_line[i] == '\0')
+		return (raw_line);
+	i++;
+	new_line = (char *)malloc((i + 1) * sizeof(char));
 	if (!new_line)
-		return (NULL);
-	i = 0;
-	while (i < length_new_line)
-	{
-		new_line[i] = return_line[i];
-		i++;
-	}
+		return (free(raw_line), NULL);
 	new_line[i] = 0;
-	// free(return_line);
+	while (i > 0)
+	{
+		i--;
+		new_line[i] = raw_line[i];
+	}
 	return (new_line);
 }
 
-char	*make_return_line(char *return_line, int fd)
+char	*make_raw_line(char *return_line, int fd)
 {
 	int		bytes_read;
 	char	*buff;
@@ -51,7 +47,7 @@ char	*make_return_line(char *return_line, int fd)
 	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
-	bytes_read = -1;
+	bytes_read = 42;
 	while (!ft_strchr(return_line, '\n') && bytes_read)	
 	{
 		bytes_read = read(fd, buff, BUFFER_SIZE);
@@ -61,32 +57,23 @@ char	*make_return_line(char *return_line, int fd)
 		return_line = ft_strjoin(return_line, buff);
 	}
 	free(buff);
-	// return_line[4] = '\0';
 	return (return_line);
 }
 
 char	*make_leftover_line(char *return_line)
 {
 	size_t	i;
-	size_t	length_new_line;
-	char	*new_line;
 
 	i = 0;
-	if (!return_line)
+	if (!return_line[0])
 		return (NULL);
 	while (return_line[i] != '\n')
 	{
 		if (return_line[i] == '\0')
-			return (return_line);
+			return (NULL);
 		i++;
 	}
-	length_new_line = ft_strlen(return_line) - i - 1;
-	new_line = (char *)malloc((length_new_line + 1) * sizeof(char));
-	new_line[0] = 0;
-	new_line = ft_strjoin(new_line, return_line + i + 1);
-	if (!new_line)
-		return (NULL);
-	return (free(return_line), new_line);
+	return (return_line + i + 1);
 }
 
 
@@ -94,40 +81,31 @@ char	*get_next_line(int fd)
 {
 	static char	*line;
 	char		*output_line;
-	char		*return_line;
 
 	if (read(fd, 0, 0) || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = make_return_line(line, fd);
-	// printf("%s", line);
+	line = make_raw_line(line, fd);
+	if (!line)
+		return (NULL);
+	// printf("rawoutput=%s\n", line);
 	output_line = make_output_line(line);
-	// printf("%s", output_line);
-	if (!output_line)
-	{
-		return (line);
-	}
+		// printf("output=%s", output_line);
 	line = make_leftover_line(line);
-	// if (!line)
-	// 	return (line);
 	return (output_line);
 }
 
+// int main(void)
+// {
+// 	int fd = open("text.txt", O_RDONLY);
+// 	char *line;
+// 	int i = 1;
 
-int main(void)
-{
-	int fd = open("text.txt", O_RDONLY);
-	char *line;
-
-	// line = get_next_line(fd);
-	// printf("%s", line);
-	// line = get_next_line(fd);
-	// printf("%s", line);
-	line = get_next_line(fd);
-	// printf("%s", line);
-	while (line)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-}
+// 	line = get_next_line(fd);
+// 	while (line)
+// 	{
+// 		printf("line %d: %s", i, line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 		i++;
+// 	}
+// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: aolde-mo <aolde-mo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:57:18 by aolde-mo          #+#    #+#             */
-/*   Updated: 2023/01/04 19:31:08 by aolde-mo         ###   ########.fr       */
+/*   Updated: 2023/01/08 15:45:21 by aolde-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,53 +22,56 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-char	*ft_calloc(size_t count, size_t size)
-{
-	size_t	i;
-	char	*ptr;
-
-	i = 0;
-	ptr = (char *)malloc(count * size);
-	if (!ptr)
-		return (NULL);
-	while (i < count * size)
-	{
-		ptr[i] = 0;
-		i++;
-	}
-	return (ptr);
-}
-
 char	*ft_strjoin(char *s1, char *s2)
 {
 	size_t	total_len;
 	char	*new_str;
 	size_t	i;
+	size_t	j;
+	int needtofree = 0;
 
 	if (!s1)
-		s1 = ft_calloc(1, sizeof(char));
-	if (!s2)
-		return (NULL);
-	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	new_str = (char *)malloc(total_len * sizeof(char));
-	if (!new_str)
-		return (0);
-	i = 0;
-	while (s1[i])
 	{
-		new_str[i] = s1[i];
-		i++;
+		s1 = (char *)malloc(sizeof(char));
+		s1[0] = 0;
+		needtofree = 1;
 	}
-	while (*s2)
+	if (!s1 || !s2)
+		return (NULL);
+	total_len = ft_strlen(s1) + ft_strlen(s2);
+	new_str = (char *)malloc((total_len + 1) * sizeof(char));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (s1)
 	{
-		new_str[i] = *s2;
+		while (s1[i])
+		{
+			new_str[i] = s1[i];
+			i++;
+		}
+	}
+	while (s2[j])
+	{
+		new_str[i] = s2[j];
 		i++;
-		s2++;
+		j++;
 	}
 	new_str[i] = 0;
+	if (needtofree)
+		free(s1);
 	return (new_str);
 }
 
+void	ft_free(char *s1, char *s2)
+{
+	if (s1)
+		free(s1);
+	if (s2)
+		free(s2);
+	return;
+}
 size_t	ft_strchr(char *s, char c)
 {
 	size_t	i;
